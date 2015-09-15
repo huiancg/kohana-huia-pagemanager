462c1b31c6d7af01dfb7ce2c4b571be25d592479
Eduardo Pacheco <eduardo.pacheco@huia.com.br>
2015-09-15 06:56:53 -0300

    - Upload de image. - Envio de bloco.

--- classes/Controller/Page/Block.php
+++ classes/Controller/Page/Block.php
@@ -11,46 +11,6 @@ class Controller_Page_Block extends Controller_App {
 		parent::before();
 	}
 
-	public function action_index()
-	{
-		if ( ! Auth::instance()->logged_in('admin'))
-		{
-			return;
-		}
-
-		$body = (array) @json_decode($this->request->body());
-		$subject = Arr::get($body, '@subject');
-		
-		if ( ! $subject)
-		{
-			return;
-		}
-		
-    preg_match('@<(\d+)\/(\d+)\/(\d+|)>@', $subject, $matchs);
-		$page_block_template_id = $matchs[1];
-		$page_id = $matchs[2];
-		$id = $matchs[3];
-		
-		$values = array();
-		foreach ($body as $key => $value)
-		{
-			preg_match('@\<http\:\/\/viejs.org\/ns\/(.*)\>@', $key, $matchs);
-			if (empty($matchs))
-			{
-				continue;
-			}
-			$key = $matchs[1];
-			$values[$key] = $value;
-		}
-
-		$model = Model_Page_Block::factory('Page_Block', $id);
-		$model->data = @json_encode($values);
-		$model->page_id = $page_id;
-		$model->page_block_template_id = $page_block_template_id;
-		$model->save();
-		$this->response->body(json_encode($model->as_array()));
-	}
-
 	public function action_new()
 	{
 		$page_id = $this->request->post('page_id');
@@ -58,14 +18,29 @@ class Controller_Page_Block extends Controller_App {
 		$this->response->body(Model_Page_Block::draft($page_id, $page_block_template_id));
 	}
 
+	public function action_upload()
+	{
+		$options = array(
+			'script_url' => URL::site('page_block/upload') . '/',
+      'upload_dir' => SYSPATH . 'public/upload/',
+      'upload_url' => URL::site('public/upload') . '/'
+		);
+		if ($_FILES AND ! isset($_FILES['files']))
+		{
+			$options['param_name'] = key($_FILES);
+		}
+		$handler = new UploadHandler($options);
+	}
+
 	public function action_save()
 	{
-		$page_id = $this->request->post('page_id');
-		$blocks = $this->request->post('blocks');
+		$page_id = $this->request->query('page_id');
+		$block = $this->request->post();
 
-		$result = Model_Page::data_save($page_id, $blocks);
+		// TODO persistir bloco
+		// $result = Model_Page::data_save($page_id, $blocks);
 
-		$this->response->json($result);
+		$this->response->json($block);
 	}
 
 }
