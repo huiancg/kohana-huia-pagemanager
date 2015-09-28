c4fe71c15c9679c8fe0f081fe9c3e20646eb06dc
Eduardo Pacheco <eduardo.pacheco@huia.com.br>
2015-09-28 11:02:15 -0300

    - Bug na alteração de dimenções do preview.

--- views/manager/page/preview.php
+++ views/manager/page/preview.php
@@ -1,12 +1,12 @@
 
-<ol class="breadcrumb">
-	<li><a href="<?php echo URL::site('manager'); ?>">Manager</a></li>
-	<li><a href="<?php echo URL::site('manager/page'); ?>">Páginas</a></li>
-	<li><?php echo $page->name; ?></li>
-</ol>
-
 <nav id="buttons-navbar" class="navbar navbar-default navbar-fixed" data-spy="affix">
   <div class="container-fluid">
+
+		<ol class="breadcrumb">
+			<li><a href="<?php echo URL::site('manager'); ?>">Manager</a></li>
+			<li><a href="<?php echo URL::site('manager/page'); ?>">Páginas</a></li>
+			<li><?php echo $page->name; ?></li>
+		</ol>
 
     <div class="btn-group" role="group">
 		  <div class="btn-group">
@@ -31,6 +31,17 @@
 		    </button>
 		  </div>
 		</div>
+
+		<div class="btn-group" role="group">
+			<div class="btn-group">
+				<button type="button" class="btn btn-default nav-btn-resolution" data-rotate="true">
+					<span class="glyphicon glyphicon-repeat"></span> Rotare
+				</button>
+				<button type="button" class="btn btn-default nav-btn-resolution">Desktop</button>
+				<button type="button" class="btn btn-default nav-btn-resolution" data-width="1024" data-height="768">Tablet</button>
+				<button type="button" class="btn btn-default nav-btn-resolution" data-width="320" data-height="568">Mobile</button>
+			</div>
+		</div>
 		
   </div>
 </nav>
@@ -48,16 +59,25 @@
 </style>
 
 <script>
+
+	var iframe_width, iframe_height = null;
+
 	var iframe_contents = document.getElementById('iframe-page').contentWindow;
 
 	var navbar = $('body > .navbar:first');
 
 	var buttons_navbar = $('#buttons-navbar');
 
+	var iframe_page = $("#iframe-page");
+
 	navbar.after(buttons_navbar);
 
 	function iframe_page_refresh() {
-		$("#iframe-page").height($("#iframe-page").contents().find("body").height());
+		var height = iframe_height || $("#iframe-page").contents().find("body").height();
+		iframe_page.height(height);
+		iframe_width = iframe_width || '100%';
+		iframe_page.width(iframe_width);
+		iframe_page.css('margin-top', buttons_navbar.height() + 20);
 	}
 
 	$("#iframe-page").load(iframe_page_refresh);
@@ -79,5 +99,19 @@
 		e.preventDefault();
 		iframe_contents._block_add_form.dialog('open');
 	});
+
+	$('.nav-btn-resolution').click(function(e) {
+		e.preventDefault();
+		var that = $(this);
+		if (that.data('rotate')) {
+			var width = iframe_width;
+			iframe_width = iframe_height;
+			iframe_height = width;
+		} else {
+			iframe_width = that.data('width');
+			iframe_height = that.data('height');
+		}
+		iframe_page_refresh();
+	})
 
 </script>
