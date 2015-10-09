f74fe7b77da12f8637be351dbf9effabc13edcc8
Eduardo Pacheco <eduardo.pacheco@huia.com.br>
2015-10-09 09:44:52 -0300

    - Filtro de blocos por página.

--- views/page/_after.php
+++ views/page/_after.php
@@ -5,7 +5,7 @@
       <fieldset>
         <label for="page_block_template_id">Template</label>
         <select name="page_block_template_id" id="page_block_template_id">
-        	<?php foreach (Model_Page_Block_Template::factory('Page_Block_Template')->find_all_ordened() as $_template) : ?>
+        	<?php foreach (Model_Page_Block_Template::factory('Page_Block_Template')->find_all_ordened($page->id) as $_template) : ?>
         	<option data-containers='<?php echo json_encode($_template->containers(), true); ?>' value="<?php echo $_template->id; ?>"><?php echo $_template->name; ?></option>
         	<?php endforeach; ?>
         </select>
