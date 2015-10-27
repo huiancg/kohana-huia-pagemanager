add04732c931ca117bcd395d740b6f41eace571a
douglas.souza <douglas.souza@huia.com.br>
2015-10-27 17:45:23 -0200

    - Adiciona método para selecionar páginas por categoria

--- classes/Model/Page/Category.php
+++ classes/Model/Page/Category.php
@@ -24,11 +24,19 @@ class Model_Page_Category extends Model_Base_Page_Category {
 	public static function products_offered()
 	{
 		$model = Model_Page_Category::factory('Page_Category');
-		// $model->where('published', '=', TRUE);
 		$model->where('slug', '!=', '');
 		$model->where('image_icon', '!=', '');
 		
 		return $model->find_all();
+	}
+
+	public static function all_pages_by_category($id)
+	{
+		$model = Model_Page_Category::factory('Page_Category', $id);
+		$pages = $model->pages;
+		$pages->where('published', '=', TRUE);
+		$pages->where('name', 'NOT LIKE', '% - Interna');
+		return $pages->find_all();
 	}
 
 }
