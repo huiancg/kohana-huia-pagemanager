afff0cc1876c2303eed613f22c64852a7ceffe1e
eduardo.pacheco <eduardo.pacheco@huia.com.br>
2016-08-29 17:45:35 -0300

    - Utilizar rota inicial como pages.

--- config/huia/base.php
+++
@@ -1,5 +0,0 @@
-<?php defined('SYSPATH') OR die('No direct access allowed.');
-
-return array(
-	'ignore_models' => array('Page'),
-);
--- init.php
+++ init.php
@@ -1,22 +1,35 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
-/*
-Route::set('pagemanager', '<catcher>', array('catcher' => '(?!^(huia|api|manager)).*'))
-	->filter(function($route, $params, $request) {
-		try
-		{
-			$exists = Model_Page::slug_exists(Arr::get($params, 'catcher'));
-			if ( ! $exists)
-			{
-				return FALSE;
-			}
-		} catch (Database_Exception $e) {} 
-	})
-	->defaults(array(
-		'controller'  => 'page',
-		'action'      => 'index',
-	));
-*/
+Route::set('manager_parent', 'manager/<parent>/<parent_id>/<controller>(/<action>(/<id>))', array(
+    'id' => '\d+',
+    'parent_id' => '\d+',
+  ))
+  ->defaults(array(
+    'controller' => 'page',
+    'action'     => 'index',
+    'directory' => 'manager'
+  ));
+  
+Route::set('manager', 'manager(/<controller>(/<action>(/<id>)))', array(
+    'id' => '\d+'
+  ))
+  ->defaults(array(
+    'controller' => 'page',
+    'action'     => 'index',
+    'directory' => 'manager'
+  ));
+  
+Route::set('manager_childs', 'manager/<parents>/<controller>(/<action>(/<id>))', array(
+    'id' => '\d+',
+    'parents' => '(?:(?!(index|edit|new)).)+',
+    'controller' => '(?:(?!(index|edit|new)).)+',
+  ))
+  ->defaults(array(
+    'controller' => 'page',
+    'action'     => 'index',
+    'directory' => 'manager'
+  ));
+
 if (ORM_Autogen::db_exists())
 {
 	Model_Page::init_routes();
