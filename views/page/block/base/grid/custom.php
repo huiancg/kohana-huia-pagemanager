32b117b269b3c7d66b4e64b193f86b38e481121f
eduardo.pacheco <eduardo.pacheco@huia.com.br>
2016-09-05 16:32:27 -0300

    - Remover instancia inválida.

--- views/page/block/base/grid/custom.php
+++ views/page/block/base/grid/custom.php
@@ -8,7 +8,7 @@ $items = explode(' ', trim($config));
 <div class="row">
   <?php foreach ($items as $index => $item) : ?>
   <div class="col-sm-12 col-md-<?php echo $item; ?>">
-    <?php echo Model_Page::instance()->blocks(Arr::get($_data, 'col-'.$index), 'col-'.$index); ?>
+    <?php echo Model_Page::factory('Page')->blocks(Arr::get($_data, 'col-'.$index), 'col-'.$index); ?>
   </div>
   <?php endforeach; ?>
 </div>
